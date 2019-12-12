50b3bd65c75e7a39d92641ba1c27f718872a7dcf
Caleb Mazalevskis <maikuolan@gmail.com>
2019-12-12 20:46:30 +0800

    Bug-fix.
    Changelog excerpt:
    
    - The directory traversal detection code was unintentionally catching
      .htaccess files, thereby causing unnecessary forced failures on some
      specific front-end operations; Fixed.

--- vault/frontend_functions.php
+++ vault/frontend_functions.php
@@ -1095,7 +1095,10 @@ $phpMussel['AppendTests'] = function (array &$Component, $ReturnState = false) u
  * @return bool True when the path is traversal-free. False when traversal has been detected.
  */
 $phpMussel['Traverse'] = function ($Path) {
-    return !preg_match('~(?:[\./]{2}|[\x01-\x1f\[-^`?*$])~i', str_replace("\\", '/', $Path));
+    return !preg_match(
+        '~(?://|(?<![\da-z])\.\.(?![\da-z])|/\.(?![\da-z])|(?<![\da-z])\./|[\x01-\x1f\[-^`?*$])~i',
+        str_replace("\\", '/', $Path)
+    );
 };
 
 /**
