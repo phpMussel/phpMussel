258020d6139a0be8641d8b148acdec3c01fe4268
Caleb Mazalevskis <maikuolan@gmail.com>
2020-08-06 01:02:40 +0800

    Bug-fix.
    Changelog excerpt:
    - Two lines in the updater code were swapped the wrong way around, which
      could cause notices to appear sometimes; Fixed.

--- vault/frontend_functions.php
+++ vault/frontend_functions.php
@@ -1534,7 +1534,7 @@ $phpMussel['UpdatesHandler-Update'] = function ($ID) use (&$phpMussel) {
             $BytesRemoved,
             $phpMussel['NumberFormatter']->format(microtime(true) - $TimeRequired, 3)
         );
-        if ($Reactivate) {
+        if (!empty($Reactivate)) {
             $phpMussel['UpdatesHandler-Activate']($ThisTarget);
         }
     }
@@ -1906,7 +1906,7 @@ $phpMussel['UpdatesHandler-Repair'] = function ($ID) use (&$phpMussel) {
             $BytesRemoved,
             $phpMussel['NumberFormatter']->format(microtime(true) - $TimeRequired, 3)
         );
-        if (!empty($Reactivate)) {
+        if ($Reactivate) {
             $phpMussel['UpdatesHandler-Activate']($ThisTarget);
         }
     }
