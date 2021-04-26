7d55e4b6a4976850bb09e855519b1131e7c134c4
Caleb Mazalevskis <maikuolan@gmail.com>
2021-04-26 20:50:16 +0800

    Refactoring.

--- vault/frontend_functions.php
+++ vault/frontend_functions.php
@@ -1076,7 +1076,7 @@ $phpMussel['UpdatesHandler'] = function ($Action, $ID = '') use (&$phpMussel) {
 
     /** Strip empty IDs. */
     if (is_array($ID)) {
-        $ID = array_filter($ID, function($Value) {
+        $ID = array_filter($ID, function ($Value) {
             return $Value !== '';
         });
     }
