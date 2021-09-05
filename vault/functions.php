d77292721206e34f72c598c5c4d8f71997ac9e1f
Caleb Mazalevskis <maikuolan@gmail.com>
2021-09-06 00:11:49 +0800

    Fallback closure patch.
    Changelog excerpt:
    - Precaution against potential future undefined index added to fallback
      closure.

--- vault/functions.php
+++ vault/functions.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Functions file (last modified: 2021.06.10).
+ * This file: Functions file (last modified: 2021.09.05).
  */
 
 /**
@@ -4636,6 +4636,9 @@ $phpMussel['Fallback'] = function (array $Fallbacks, array &$Config) use (&$phpM
             }
             if (isset($Dir)) {
                 unset($Dir);
+            }
+            if (!isset($Cat[$DKey])) {
+                $Cat[$DKey] = '';
             }
             $Dir = &$Cat[$DKey];
             if (isset($DData['value_preg_filter']) && is_array($DData['value_preg_filter'])) {
