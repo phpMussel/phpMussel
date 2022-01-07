0ef86633a0205ae345da9886379484f074b301f3
Caleb Mazalevskis <maikuolan@gmail.com>
2022-01-08 02:15:57 +0800

    Aesthetic patch.

--- vault/fe_assets/pips.php
+++ vault/fe_assets/pips.php
@@ -8,7 +8,7 @@
  * License: GNU/GPLv2
  * @see LICENSE.txt
  *
- * This file: Default theme pips handler (last modified: 2021.11.30).
+ * This file: Default theme pips handler (last modified: 2021.12.11).
  */
 
 /** Prevents execution from outside of phpMussel. */
@@ -18,8 +18,8 @@ if (!defined('phpMussel')) {
 
 $phpMussel['FE']['PIP_Key'] = 'R0lGODlhBwAJAKECAJkCAP3/+////////yH5BAEKAAEALAAAAAAHAAkAAAINjH+gyaaAAkQrznRbKAA7';
 $phpMussel['FE']['PIP_Left'] = 'R0lGODlhCAAIAKECAJkCAPv99P///////yH5BAEKAAEALAAAAAAIAAgAAAINjH+ga6vJIEDh0UmzKQA7';
-$phpMussel['FE']['PIP_Left_Invalid'] = 'R0lGODlhCAASAKECAJkCAAKZAAAAAAAAACH5BAEKAAEALAAAAAAIABIAAAIXjH+ga6vJIEDh0UmzzhZ1yGGeuIEXqRUAOw==';
-$phpMussel['FE']['PIP_Left_Valid'] = 'R0lGODlhCAASAKECAJkCAAKZAAAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIblH+ga6vJIEDi0Umz3jzk4FEhAh7laIBoyh0FADs=';
+$phpMussel['FE']['PIP_Left_Invalid'] = 'R0lGODlhCAAQAIABAJkCAAAAACH5BAEKAAEALAAAAAAIABAAAAIWjH+ga6vJIEDh0Ulhtojri2Vb+IlGAQA7';
+$phpMussel['FE']['PIP_Left_Valid'] = 'R0lGODlhCAAQAKECAJkCAAKZAAAAAAAAACH5BAEKAAIALAAAAAAIABAAAAIalH+ga6vJIEDi0UmzFiGHToHId5Ci8Z3ohhQAOw==';
 $phpMussel['FE']['PIP_Right'] = 'R0lGODlhCAAIAKECAJkCAPv99P///////yH5BAEKAAEALAAAAAAIAAgAAAINjH+gmwvoUGBSSfOuKQA7';
-$phpMussel['FE']['PIP_Right_Invalid'] = 'R0lGODlhCAASAKECAJkCAAKZAAAAAAAAACH5BAEKAAEALAAAAAAIABIAAAIYjH+gmwvoUGBSSfOuvrZm+DVcuGHkdEoFADs=';
-$phpMussel['FE']['PIP_Right_Valid'] = 'R0lGODlhCAASAKECAJkCAAKZAAAAAAAAACH5BAEKAAIALAAAAAAIABIAAAIblH+gmwvokGBSSfOu3jzc4EkhAh7laIBoyh0FADs=';
+$phpMussel['FE']['PIP_Right_Invalid'] = 'R0lGODlhCAAQAIABAJkCAAAAACH5BAEKAAEALAAAAAAIABAAAAIWjH+gmwvoUGBSSfPatTVDr10i94lIAQA7';
+$phpMussel['FE']['PIP_Right_Valid'] = 'R0lGODlhCAAQAKECAJkCAAKZAAAAAAAAACH5BAEKAAIALAAAAAAIABAAAAIalH+gmwvokGBSSfOuFuGGLoHId5Ci8Z3ohhQAOw==';
