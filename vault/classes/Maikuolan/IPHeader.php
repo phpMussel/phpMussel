1700a4e61654e372d9a8baaf2e8629a1cb568023
Caleb Mazalevskis <maikuolan@gmail.com>
2023-01-23 00:43:13 +0800

    Common classes update.

--- vault/classes/Maikuolan/IPHeader.php
+++ vault/classes/Maikuolan/IPHeader.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * IP header class (last modified: 2022.11.22).
+ * IP header class (last modified: 2023.01.22).
  *
  * This file is a part of the "common classes package", utilised by a number of
  * packages and projects, including CIDRAM and phpMussel.
@@ -37,7 +37,7 @@ class IPHeader
      *      be needed by some implementations to ensure compatibility).
      * @link https://github.com/Maikuolan/Common/tags
      */
-    const VERSION = '1.9.3';
+    const VERSION = '1.9.4';
 
     /**
      * Constructor.
--- vault/classes/Maikuolan/NumberFormatter.php
+++ vault/classes/Maikuolan/NumberFormatter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Number formatter (last modified: 2022.11.22).
+ * Number formatter (last modified: 2023.01.22).
  *
  * This file is a part of the "common classes package", utilised by a number of
  * packages and projects, including CIDRAM and phpMussel.
@@ -1068,7 +1068,7 @@ class NumberFormatter
      *      be needed by some implementations to ensure compatibility).
      * @link https://github.com/Maikuolan/Common/tags
      */
-    const VERSION = '1.9.3';
+    const VERSION = '1.9.4';
 
     /**
      * Constructor.
